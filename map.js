280af12aee877ff505f58f74f607dd9cabd8954b
Lucas Almeida <lalmeid2@lion.lmu.edu>
2016-06-08 15:30:47 -0700

    Markers positioned on the map.
    Finally got to position more than one marker (which indicates where the
    air quality egg is) on the map.

--- map.js
+++ map.js
@@ -1,48 +1,59 @@
-//Set the Google Maps' map
-function initialize() {
-    var mapProp = {
-        center:new google.maps.LatLng(33.9689978832525,-118.415820072074),
-        zoom:13,
-        mapTypeId:google.maps.MapTypeId.ROADMAP
-    };
-    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+// //Set the Google Maps' map
+// function initialize() {
+//     var mapProp = {
+//         center:new google.maps.LatLng(33.9689978832525,-118.415820072074),
+//         zoom:13,
+//         mapTypeId:google.maps.MapTypeId.ROADMAP
+//     };
+//     var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+// }
+// google.maps.event.addDomListener(window, 'load', initialize);
 
-//Indicating where the Air Quality Egg is on the map.
-xivelyGmaps.setMapElement("#googleMap");
-xivelyGmaps.setKey("U5o8w9lgaTX9Gkd89zlCM4NCvrnnVj6cEUJIKHcZ5MIW1Ijw");
-google.maps.event.addDomListener(google.maps.Marker, 'load', xivelyGmaps.subscribe("967735657"));
-//google.maps.event.addDomListener(google.maps.Marker, 'load', xivelyGmaps.subscribe("485551354"));
+// //Indicating where the Air Quality Egg is on the map.
+// xivelyGmaps.setMapElement("#googleMap");
+// xivelyGmaps.setKey("U5o8w9lgaTX9Gkd89zlCM4NCvrnnVj6cEUJIKHcZ5MIW1Ijw");
+// google.maps.event.addDomListener(google.maps.Marker, 'load', xivelyGmaps.subscribe("967735657"));
+// //google.maps.event.addDomListener(google.maps.Marker, 'load', xivelyGmaps.subscribe("485551354"));
 
-// var locations = [
-//       ['Bondi Beach', -33.890542, 151.274856, 4],
-//       ['Coogee Beach', -33.923036, 151.259052, 5],
-//       ['Cronulla Beach', -34.028249, 151.157507, 3],
-//       ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-//       ['Maroubra Beach', -33.950198, 151.259302, 1]
-//     ];
 
-//     var map = new google.maps.Map(document.getElementById('googleMap'), {
-//       zoom: 10,
-//       center: new google.maps.LatLng(-33.92, 151.25),
-//       mapTypeId: google.maps.MapTypeId.ROADMAP
-//     });
+xively.setKey( "U5o8w9lgaTX9Gkd89zlCM4NCvrnnVj6cEUJIKHcZ5MIW1Ijw" ); 
 
-//     var infowindow = new google.maps.InfoWindow();
+var setMarkerOnMap = function (feedID) {
+    var coordinates = {name: "", lat: 0, lon: 0};
+    var marker;
 
-//     var marker, i;
+    //Get datastream data from Xively  
+	xively.feed.get(feedID, function (feed) {
+		coordinates.name = feed["title"];
+		coordinates.lat = feed["location"].lat;
+		coordinates.lon = feed["location"].lon;
 
-//     for (i = 0; i < locations.length; i++) { 
-//       marker = new google.maps.Marker({
-//         position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-//         map: map
-//       });
+		marker = new google.maps.Marker({
+	    	position: new google.maps.LatLng(coordinates.lat, coordinates.lon),
+	    	map: map
+	  	});
 
-//       google.maps.event.addListener(marker, 'click', (function(marker, i) {
-//         return function() {
-//           infowindow.setContent(locations[i][0]);
-//           infowindow.open(map, marker);
-//         }
-//       })(marker, i));
-//     }
+	  	google.maps.event.addListener(marker, 'click', (function(marker, i) {
+	    	return function() {
+	      	infowindow.setContent(coordinates.name);
+	      	infowindow.open(map, marker);
+	    	}
+	  	})(marker, i));
+	});
+};
+
+
+var feedID = 967735657;
+
+var map = new google.maps.Map(document.getElementById('googleMap'), {
+  	zoom: 13,
+  	center: new google.maps.LatLng(33.9689978832525,-118.415820072074),
+ 	mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+
+var infowindow = new google.maps.InfoWindow();
+
+var i;
+
+setMarkerOnMap(feedID);
+setMarkerOnMap(485551354);
