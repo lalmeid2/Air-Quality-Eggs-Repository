a844c0ee52c22e5edaa9a157f91992831e7f3717
Lucas Almeida <lalmeid2@lion.lmu.edu>
2016-06-09 15:47:00 -0700

    Showing the data from the eggs on the page.

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 //The key has to be set before trying to retrieve data from Xively
 xively.setKey( "U5o8w9lgaTX9Gkd89zlCM4NCvrnnVj6cEUJIKHcZ5MIW1Ijw" ); 
 
-var setDataOnPage = function (feedID) {
+var setDataOnPage = function (feedID, divID) {
     var coordinates = {name: "", lat: 0, lon: 0};
     var marker;
 
@@ -11,15 +11,28 @@ var setDataOnPage = function (feedID) {
         coordinates.lat = feed["location"].lat;
         coordinates.lon = feed["location"].lon;
 
+        var out = "<table><tr><th>AQE: " + feed["title"] + "</th><th>Value</th></tr>";
+
         //Structure used to get the sensor type (string) and its value:
         for (var i = 0; i < feed["datastreams"].length; i++) {
             for (var j = 0; j < feed["datastreams"][i]["tags"].length; j++) {
                 if (feed["datastreams"][i]["tags"][j].indexOf("aqe:sensor_type") != -1) {
                     //console.log(feed["datastreams"][i]["tags"][j].slice(16) + " " + feed["datastreams"][i]["current_value"]);
-                    console.log(feed["datastreams"][i]["id"] + " " + feed["datastreams"][i]["current_value"]);
+                    //console.log(feed["datastreams"][i]["id"] + " " + feed["datastreams"][i]["current_value"]);
+                
+                    out += "<tr><td>" + 
+                    feed["datastreams"][i]["id"] +
+                    "</td><td>" +
+                    feed["datastreams"][i]["current_value"] +
+                    "</td><tr>";
+                    
                 };
             };
         };
+
+        out += "</table>";
+        document.getElementById(divID).innerHTML = out;
+
     });
 };
 
@@ -39,7 +52,23 @@ var feedIDs = [967735657,
               908476336,
               329270064];
 
+var divIDs = ["id01",
+              "id02",
+              "id03",
+              "id04",
+              "id05",
+              "id06",
+              "id07",
+              "id08",
+              "id09",
+              "id010",
+              "id011",
+              "id012",
+              "id013",
+              "id014",
+              "id015"];
+
 //Loop to set the markers on the map
 for (var i = 0; i < feedIDs.length; i++) {
-    setDataOnPage(feedIDs[i]);
+    setDataOnPage(feedIDs[i], divIDs[i]);
 };
