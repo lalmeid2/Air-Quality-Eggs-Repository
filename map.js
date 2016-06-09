4f68245718ff496ba95d4e93455d82f3d4496fc9
Lucas Almeida <lalmeid2@lion.lmu.edu>
2016-06-09 09:52:31 -0700

    Using a loop to put the markers on the map;

--- map.js
+++ map.js
@@ -26,7 +26,21 @@ var setMarkerOnMap = function (feedID) {
 };
 
 
-var feedID = 967735657;
+var feedIDs = [967735657,
+			  485551354,
+			  1844507298,
+			  2035052636,
+			  163188704,
+			  294678862,
+			  391882660,
+			  428399050,
+			  873272112,
+			  2045393884,
+			  1700345296,
+			  1218314598,
+			  2087285270,
+			  908476336,
+			  329270064];
 
 var map = new google.maps.Map(document.getElementById('googleMap'), {
   	zoom: 15,
@@ -36,22 +50,7 @@ var map = new google.maps.Map(document.getElementById('googleMap'), {
 
 var infowindow = new google.maps.InfoWindow();
 
-var i;
-
-//Needs a loop; trying to do it with files;
-
-setMarkerOnMap(967735657);
-setMarkerOnMap(485551354);
-setMarkerOnMap(1844507298);
-setMarkerOnMap(2035052636);
-setMarkerOnMap(163188704);
-setMarkerOnMap(294678862);
-setMarkerOnMap(391882660);
-setMarkerOnMap(428399050);
-setMarkerOnMap(873272112);
-setMarkerOnMap(2045393884);
-setMarkerOnMap(1700345296);
-setMarkerOnMap(1218314598);
-setMarkerOnMap(2087285270);
-setMarkerOnMap(908476336);
-setMarkerOnMap(329270064);
+//Loop to set the markers on the map
+for (var i = 0; i < feedIDs.length; i++) {
+	setMarkerOnMap(feedIDs[i]);
+};
