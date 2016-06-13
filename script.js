d6dd643bb33153692d18d837f082489f49d88fe0
Lucas Almeida <lalmeid2@lion.lmu.edu>
2016-06-13 15:10:46 -0700

    Changing the script.js file because of the name changing of eggs_data file.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
 "<li><a href='index.html'>Map</a></li>" +
-"<li><a href='customers.html'>Data</a></li>" +
+"<li><a href='eggs_data.html'>Data</a></li>" +
 "<li><a href='about.html'>About</a></li>" +
 "</ul>";
