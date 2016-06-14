4fbc3f44cc34f4e7e903198f5a972a8b87bbc34f
Lucas Almeida <lalmeid2@lion.lmu.edu>
2016-06-14 11:29:54 -0700

    Adjusting the "feedIDs" array.
    Including the names of the Air Quality Eggs and putting them in order.

--- data.js
+++ data.js
@@ -31,31 +31,29 @@ var setDataOnPage = function (feedID, divID) {
                 };
             };
         };
-
         out += "</table><p></p>";
         div.innerHTML = out;
         document.body.appendChild(div);
-
     });
 };
 
 var feedIDs = [
-              1218314598,   //LMU-nj 1
-              329270064,    //LMU-nj 2
-              1700345296,   //LMU-nj 3
-              2045393884,   //LMU-nj 4
-              2087285270,   //LMU-nj 5
-              967735657,    //LMU-NJ 6
-              428399050,    //LMU-NJ 7
-              908476336,    //LMU-nj 8
-              294678862,    //LMU-NJ 15
-              873272112,    //LMU-NJ 16
-              391882660,    //LMU-NJ 18
-              1844507298,   //JML001
-              2035052636,   //JML002
-              485551354,    //JML003
-              163188704     //LMU Air Quality Egg
-              ];
+                1218314598,   //LMU-nj 1
+                329270064,    //LMU-nj 2
+                1700345296,   //LMU-nj 3
+                2045393884,   //LMU-nj 4
+                2087285270,   //LMU-nj 5
+                967735657,    //LMU-NJ 6
+                428399050,    //LMU-NJ 7
+                908476336,    //LMU-nj 8
+                294678862,    //LMU-NJ 15
+                873272112,    //LMU-NJ 16
+                391882660,    //LMU-NJ 18
+                1844507298,   //JML001
+                2035052636,   //JML002
+                485551354,    //JML003
+                163188704     //LMU Air Quality Egg
+            ];
 
 //Loop to set the markers on the map
 for (var i = 0; i < feedIDs.length; i++) {
